b2f231332a7f7d3d8dd6951dceedcc7d17144e36
LuisLourenco1 <luislourenco@estudante.ufscar.br>
2024-09-13 00:04:22 -0300

    Commit Final

---
+++ README.md
@@ -0,0 +1,67 @@
+# Introdução
+
+Neste trabalho, vamos juntar todas as camadas que implementamos até agora e depois testar o funcionamento em uma rede física real!
+
+
+# Por que o grupo é de até 10 pessoas?
+
+Para vocês terem a liberdade de testar várias implementações diferentes de P1, P2, P3 e P4, afinal esse trabalho consiste em juntar as diversas camadas.
+
+As implementações não precisam estar perfeitas para funcionarem em conjunto:
+
+ * Teoricamente, uma implementação de TCP passando até o teste do Passo 3 é suficiente para estabelecer uma conexão, trocar dados e comprovar o funcionamento. Uma implementação de TCP passando até o teste do Passo 4 deve funcionar quase perfeitamente, pois a nossa camada física é bastante confiável e tem probabilidade baixíssima de perder ou corromper pacotes.
+
+ * Uma implementação de IP passando até o teste do Passo 3 deve funcionar bem se você não alterar as tabelas de encaminhamento fornecidas (se alterar, o Passo 4 é importante para evitar loops de roteamento). Você só vai precisar do Passo 5 do IP se quiser testar o mtr ou traceroute.
+
+No entanto, a implementação de vocês pode ter erros que, por azar, não foram exercitados pelos testes automatizados. Portanto, dica valiosa: testem todas as combinações possíveis de implementações dos seus integrantes, até funcionar.
+
+
+# Por que a Zybo Z7-20 e não um PC?
+
+Nós configuramos a Zybo Z7-20 para disponibilizar 8 interfaces seriais em cada placa. Assim, conseguimos montar redes usando uma camada física simples e fácil de entender.
+
+![](fig/portas.jpg)
+
+
+# Configuração sugerida
+
+
+![](fig/diagrama.svg)
+
+
+# Orientações gerais
+
+Como as placas são compartilhadas, algumas **orientações gerais muito importantes**:
+
+ * Crie um diretório para o seu grupo trabalhar.
+
+ * Não bisbilhote o diretório dos outros grupos.
+
+
+# Instruções
+
+Copie para o diretório do seu grupo, em cada placa, os arquivos:
+
+ * [tcputils.py](https://github.com/thotypous/redes-t2-grader/blob/main/tcputils.py)
+ * [iputils.py](https://github.com/thotypous/redes-t3-grader/blob/main/iputils.py)
+ * [camadafisica.py](camadafisica.py)
+ * Os arquivos `tcp.py`, `ip.py` e `slip.py` que vocês implementaram no P2, P3 e P4.
+
+Copie também o executável principal que você vai executar em cada placa, respectivamente:
+
+ * [placa1.py](placa1.py)
+ * [placa2.py](placa2.py)
+ * [placa3.py](placa3.py)
+
+Execute o executável principal em cada placa.
+
+Na placa 1, você vai ter que executar, em outros terminais (sem matar o Python!), alguns comandos que o executável principal vai indicar. Obs.: é normal aparecer uma vez a mensagem `BlockingIOError: [Errno 11] Resource temporarily unavailable` depois de executar o `slattach`.
+
+Teste se o servidor de eco está funcionando: conecte-se a ele a partir da placa 1 executando:
+```
+nc 192.168.200.4 7000
+```
+
+Se tiver problemas, pode ser útil fazer algumas simulações locais (veja os arquivos `exemplo_integracao.py` que foram fornecidos ao longo dos trabalhos anteriores), pois é mais fácil depurar localmente do que nas placas.
+
+Uma vez com o servidor de eco funcionando, edite o arquivo `placa3.py` e tente inserir sua implementação de camada de aplicação do P1 (servidor de IRC).
---
+++ placa3.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+import asyncio
+from camadafisica import ZyboSerialDriver
+from tcp import Servidor        # copie o arquivo do T2
+from ip import IP               # copie o arquivo do T3
+from slip import CamadaEnlace   # copie o arquivo do T4
+
+import re
+ 
+## Implementação da camada de aplicação
+
+def validar_nome(nome):
+    return re.match(br'^[a-zA-Z][a-zA-Z0-9_-]*$', nome) is not None
+
+def sair(conexao):
+    print(conexao, 'conexão fechada')
+    
+    # Itera pelos canais em que o usuário está
+    if hasattr(conexao, 'canais'):
+        for nome_canal in conexao.canais:
+            if conexao in canais[nome_canal.lower()]:
+                canais[nome_canal.lower()].remove(conexao) # Remove o usuário da lista de nomes do canal
+            
+            for user in canais[nome_canal.lower()]: # Itera pelos usuários de cada canal em que o usuário está
+                user.enviar(b':%s QUIT :Connection closed\r\n' % conexao.nick)
+            
+    # Remove o nome do usuário da lista de apelidos
+    if conexao.nick.lower() != b'':
+        del apelidos[conexao.nick.lower()]
+
+    conexao.fechar()
+
+def getComando(string, comando):
+    return string.startswith(comando)
+
+def dados_recebidos(conexao, dados):
+    if dados == b'':
+        return sair(conexao)
+    
+    # Separa os comandos (b'\r\n') sem apagar os b'\r\n'
+    pattern = b'(?<=\r\n)'
+    dados = [substring for substring in re.split(pattern, dados)]
+    
+    
+    for d in dados: # Itera sob cada comando
+        if d.endswith(b'\r\n'): # Processa os comandos bem formados
+            d = conexao.residuais + d # Concatena os residuais com os dados
+            
+            
+            # Comando PING
+            if getComando(d, b'PING'):
+                d = d.replace(b'PING ', b'')
+                conexao.enviar(b':server PONG server :' + d)
+
+
+            # Comando NICK
+            if getComando(d, b'NICK'):
+                nick = d.replace(b'NICK ', b'').replace(b'\r\n', b'')
+                
+                # Caso 1: Nome válido
+                if validar_nome(nick):
+                    
+                    # Caso 1.1: Nick NÃO-disponível (não Case-Sensitive)
+                    if nick.lower() in apelidos:
+                        
+                        # Caso 1.1.1: 1o acesso
+                        if conexao.nick == b'':
+                            conexao.enviar(b':server 433 * %s :Nickname is already in use\r\n' % nick)
+                            
+                        # Caso 1.1.2: Troca
+                        else:
+                            conexao.enviar(b':server 433 %s %s :Nickname is already in use\r\n' % (conexao.nick, nick))
+                    
+                    # Caso 1.2: Nick disponível    
+                    else:
+                        # Caso 1.2.1: 1o acesso
+                        if conexao.nick == b'':
+                            conexao.enviar(b':server 001 %s :Welcome\r\n' % nick)
+                            conexao.enviar(b':server 422 %s :MOTD File is missing\r\n' % nick)
+                            
+                        # Caso 1.2.2: Troca
+                        else:
+                            conexao.enviar(b':%s NICK %s\r\n' % (conexao.nick, nick))
+                            del apelidos[conexao.nick.lower()] # Apaga o nome antigo do dicionário
+                            # Se não apagar, durante o 'if valor in apelidos.values()' pode achar a conexão antiga
+                            
+                        # Registra independente do caso
+                        apelidos[nick.lower()] = conexao # Associa o nome e a conexão
+                        conexao.nick = nick # Registra o nome da conexão no objeto
+
+                # Caso 2: Nome inválido
+                else:
+                    conexao.enviar(b':server 432 * %s :Erroneous nickname\r\n' % nick)
+                    
+                    
+            # Comando PRIVMSG
+            if getComando(d, b'PRIVMSG'):
+                priv = d.replace(b'PRIVMSG ', b'').replace(b'\r\n', b'')
+                dest, msg = priv.split(b' :', 1)
+                
+                # Caso 1: Usuário
+                if dest.lower() in apelidos:
+                    apelidos[dest.lower()].enviar(b':%s PRIVMSG %s :%s\r\n' % (conexao.nick, dest, msg))
+                
+                # Caso 2: Canal
+                if dest.startswith(b'#'):
+                    nome_canal = dest.replace(b'#', b'')
+                    
+                    # Verifica se o usuário está no canal
+                    if conexao in canais[nome_canal.lower()]:
+                        # Manda para todos os usuários que estão naquela canal
+                        for user in canais[nome_canal.lower()]:
+                            if user != conexao:
+                                user.enviar(b':%s PRIVMSG %s :%s\r\n' % (conexao.nick, b'#' + nome_canal, msg))
+
+     
+            # Comando JOIN
+            if getComando(d, b'JOIN'):
+                nome_canal = d.replace(b'JOIN ', b'').replace(b'\r\n', b'')
+                
+                # Caso 1: Nome do canal válido
+                if validar_nome(nome_canal.replace(b'#', b'')) and nome_canal.startswith(b'#'):
+                    nome_canal = nome_canal.replace(b'#', b'')
+                    
+                    # Canal não existe ainda     
+                    if nome_canal.lower() not in canais:
+                        canais[nome_canal.lower()] = [] # Cria a lista de nomes
+                    
+                    # Usuário não está no canal ainda
+                    if conexao not in canais[nome_canal.lower()]:
+                        canais[nome_canal.lower()].append(conexao) # Registra a conexão do usuário no canal
+                        
+                        # Envia a mensagem de JOIN para todos os usuários do canal
+                        for user in canais[nome_canal.lower()]:
+                            user.enviar(b':%s JOIN :%s\r\n' % (conexao.nick, b'#' + nome_canal))
+                    
+                    # Registra o canal na lista de canais da conexão
+                    if nome_canal.lower() not in conexao.canais:
+                        conexao.canais.append(nome_canal.lower())
+                        
+                    # Passo 8
+                    lista_nomes = [user.nick for user in canais[nome_canal.lower()]]
+                    lista_nomes = b' '.join(sorted(lista_nomes))
+                    
+                    # Envia a lista e o fim da lista para o novo membro do canal
+                    conexao.enviar(b':server 353 %s = %s :%s\r\n' % (conexao.nick, b'#' + nome_canal, lista_nomes))
+                    conexao.enviar(b':server 366 %s %s :End of /NAMES list.\r\n' % (conexao.nick, b'#' + nome_canal))
+                
+                # Caso 2: Nome do canal inválido
+                else:
+                    conexao.enviar(b':server 403 canal :No such channel\r\n')
+                    
+            
+            # Comando PART
+            if getComando(d, b'PART'):
+                # Remove tudo, inclusive a mensagem de partida, deixando apenas o nome do canal
+                nome_canal = d.replace(b'PART ', b'').replace(b'\r\n', b'').split(b' :', 1)[0]
+                
+                # Se o canal começa com #
+                if nome_canal.startswith(b'#'):
+                    nome_canal = nome_canal.replace(b'#', b'')
+                    
+                    # Se o canal existe, se o usuário está no canal
+                    if nome_canal.lower() in canais and nome_canal.lower() in conexao.canais:
+                        
+                        # Envia para todos os usuários do canal
+                        for user in canais[nome_canal.lower()]:
+                            user.enviar(b':%s PART %s\r\n' % (conexao.nick, b'#' + nome_canal))
+                            
+                        # Remove a conexão do canal e o canal da lista da conexão
+                        canais[nome_canal.lower()].remove(conexao)
+                        conexao.canais.remove(nome_canal.lower())
+        
+        
+                
+            # Printa
+            print(conexao, d)
+            
+            # Reseta o resíduo
+            conexao.residuais = b''
+            
+        else:
+            # Concatena os dados que não terminam com b'\n' e salva nos residuais
+            conexao.residuais += d
+
+    # Caso os resíduos se completem e não hajam mais mensagens em seguida
+    if conexao.residuais.endswith(b'\r\n'):
+        conexao.enviar(b':server PONG server :' + conexao.residuais.replace(b'PING ', b'')) # Remove o comando PING
+        conexao.residuais = b''
+        
+        print(conexao, d)
+
+
+
+def conexao_aceita(conexao):
+    # Passo 2
+    conexao.residuais = b''
+    
+    # Passo 4
+    conexao.nick = b''
+    
+    # Passo 5
+    conexao.canais = []
+    
+    print(conexao, 'nova conexão')
+    conexao.registrar_recebedor(dados_recebidos)
+    
+
+# Lista de nomes
+apelidos = {}
+
+# Lista de canais
+canais = {}
+
+
+## Integração com as demais camadas
+
+nossa_ponta = '192.168.200.4'
+outra_ponta = '192.168.200.3'
+porta_tcp = 7000
+
+driver = ZyboSerialDriver()
+linha_serial = driver.obter_porta(0)
+
+enlace = CamadaEnlace({outra_ponta: linha_serial})
+rede = IP(enlace)
+rede.definir_endereco_host(nossa_ponta)
+rede.definir_tabela_encaminhamento([
+    ('0.0.0.0/0', outra_ponta)
+])
+servidor = Servidor(rede, porta_tcp)
+servidor.registrar_monitor_de_conexoes_aceitas(conexao_aceita)
+asyncio.get_event_loop().run_forever()
